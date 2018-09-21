59780f9229ca66bf012133c1b30e29f439cc385e
Foram Patel <fpatel1810@gmail.com>
2018-09-20 20:50:46 -0400

    Initial Commit

---
+++ newone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newone
+//
+//  Created by newuser on 2018-09-20.
+//  Copyright © 2018 newuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
